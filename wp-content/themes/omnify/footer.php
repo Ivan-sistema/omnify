6a1d521170a12b5aa991c03091cc801cb8e1cc67
Ivan dos santos <ivantkd1@gmail.com>
2021-03-25 02:25:51 -0300

    [OM-2] - Create header

--- wp-content/themes/omnify/footer.php
+++ wp-content/themes/omnify/footer.php
@@ -1,7 +1,5 @@
 <?php wp_footer(); ?>
 
 <section id="footer">
-<?php get_template_part('template_parts/parts/orcamento'); ?>
-<?php get_template_part('template_parts/parts/parceiro'); ?>
-<?php get_template_part('template_parts/parts/copy'); ?>
+<?php // get_template_part('template_parts/parts/copy'); ?>
 </section>
--- wp-content/themes/omnify/template_parts/parts/banner.php
+++ wp-content/themes/omnify/template_parts/parts/banner.php
@@ -1,39 +1,5 @@
-<section id="banner"  class="section">
-    <div class="container-site">
-        <div class="headline">
-            <div class="headline__logotipo">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logotipo.png">
-            </div>
-            <div class="headline__title">
-                <h2>Reformas completas arquitetônicas</h2>
-            </div>
-
-            <div class="headline__content">
-                <p>Você já pensou na facilidade que seria em ter toda a mão de obra em um só lugar?</p>
-            </div>
-            <div class="headline__button">
-                <a href="#orcamento">Solicite um orçamento</a>
-            </div>
-        </div>
-        <div class="action">
-            <a href="#about">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icones/angle-double-down.png">
-            </a>
-        </div>
+<section id="banner">
+    <div class="b_headline">
+        
     </div>
-    
-    <div class="action-mobile">
-            <a hre="https://api.whatsapp.com/send?phone=5511975168813" target="_blank" title="Reformarq | Whatsapp">
-                <div class="top-action">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icones-mobile/poligono.png">
-                </div>
-                <div class="content-action">
-                    <div class="box-action">
-                    <p>Clique aqui e receba um<br/> orçamento rápido e gratuito!</p>
-                    <div class="content-image">
-                    <img class="img__config img__config--whatsapp" src="<?php echo get_template_directory_uri(); ?>/assets/images/icones-mobile/whatsapp.png" alt="Reformarq | Whatsapp">
-</div></div>
-                </div>
-            </a>
-        </div>
 </section>
