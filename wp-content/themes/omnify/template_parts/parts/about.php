d3369b18a7caea9572cd7beb79812b5e2d58f684
Ivan dos santos <ivantkd1@gmail.com>
2021-03-25 22:13:28 -0300

    [TASk/OM-8] - Implement Section Service

---
+++ wp-content/themes/omnify/template_parts/parts/about.php
@@ -0,0 +1,32 @@
+<section id="about">
+    <div class="about__container">
+        <div class="matter">
+            <h2>Why Lighting Matters</h2>
+            <p>Omnify is more than a leading LED backlighting provider – it is an attitude of possibility.</p>
+            <p>We exist to enable your visionary design for people and product.</p> 
+            <p>There’s a better way of backlighting things – through the elevation of your creativity.</p>
+        </div>
+        <div class="focus">
+            <div class="focus__images">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-focus.png">
+            </div>
+            <div class="focus__description">
+                <div class="desc">
+                    <h2>The Focus</h2>
+                    <p>Shaped and indirect illumination can be a focal point that defines a space, capturing the minds of audiences and holding their attention.</p>
+                </div>
+            </div>
+        </div>
+        <div class="background">
+            <div class="background__description">
+                <div class="desc">
+                    <h2>The Background</h2>
+                    <p>Thoughtful lighting can work as the backdrop to the design and layout of a space, providing a vital sense of realism, mood and tension.</p>
+                </div>
+            </div>
+            <div class="background__images">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-background.png">
+            </div>
+        </div>
+    </div>
+</section>
