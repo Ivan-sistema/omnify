29d718dd7748fb83229a576051e0dd52c5c7ebe0
Ivan dos santos <ivantkd1@gmail.com>
2021-03-26 00:07:42 -0300

    [TASk/OM-9] - Create and implement section innovation

--- wp-content/themes/omnify/index.php
+++ wp-content/themes/omnify/index.php
@@ -4,5 +4,6 @@
 <?php get_template_part('template_parts/parts/banner'); ?>
 <?php get_template_part('template_parts/parts/about'); ?>
 <?php get_template_part('template_parts/parts/service'); ?>
+<?php get_template_part('template_parts/parts/innovation'); ?>
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/omnify/template_parts/parts/innovation.php
@@ -0,0 +1,19 @@
+<section id="innovation" class="bg-innovation" style="background: url('<?php echo get_template_directory_uri(); ?>/assets/images/innovation/bg-innovation.png') center center no-repeat">
+    <div class="innovation-container">
+        <div class="innovation-container__header">
+            <h2>Commitment to Innovation</h2>
+            <p>Our pursuit is, and always has been, simple: to fuse sophisticated LED lighting design with unparalleled craftsmanship in a way that’s endlessly customizable in form and function.</p>
+            <small>We like to think that creativity is just intelligence having fun: we like having fun.</small>
+        </div>
+        <div class="innovation-container__footer">
+        <div class="certification">
+            <p>All our products bear the UL mark for safety</p>
+        </div>
+        <div class="certification-r">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/innovation/logo-lumisheet.png">
+            <p>Nothing speaks to creativity more than LumiSheet: the pinnacle of LED light guided panels for over 20 years.<br />
+            <a href="#">Learn More</a></p>
+        </div>
+        </div>
+    </div>
+</section>
