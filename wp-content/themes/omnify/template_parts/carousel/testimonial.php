dc271aeeddf7ee39a77f6de532b5b6143ca835a5
Ivan dos santos <ivantkd1@gmail.com>
2021-03-28 15:02:16 -0300

    [TASK/OM-13] - Create testimonials and blog

---
+++ wp-content/themes/omnify/template_parts/carousel/testimonial.php
@@ -0,0 +1,17 @@
+<div class="owl-carousel owl-carousel-testimonial owl-theme">
+
+    <div class="testimonial__carousel">
+        
+        <div class="item" id="carousel">
+
+            <div class="post__description">
+                <p>“The beautiful thing about Omnify is that their solutions are built just for you; fully custom. They don’t require extensive physical modifications to be installed, they work around the existing layout delivering the brightest and sharpest edge-to-edge performance of any lighting solution out there—and all products come backed by an unparalleled warranty which is backed by the expertise and support Omnify brings to the table.”</p>
+            </div>
+            <div class="post__user">
+                <i class="fas fa-user"></i> <hr><p>John Grayson</p>
+            </div>
+
+        </div>
+    </div>	      
+</div>	
+
