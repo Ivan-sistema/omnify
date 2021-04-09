d8c30b0f3b8b02149a7898a6408c05b0717c0122
Ivan dos santos <ivantkd1@gmail.com>
2021-04-09 11:30:28 -0300

    years

---
+++ wp-content/themes/omnify/template_parts/parts/years.php
@@ -0,0 +1,8 @@
+<section id="years" class="years">
+    <div class="years-container">
+        <div class="years-container__number">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/years/20.png" alt="">
+        </div>
+        <div class="years-container__description">\zx\x\z</div>
+    </div>
+</section>
